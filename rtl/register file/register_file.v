0242ebd6efcefe88c9035a5dca73cbf406b9f71d
maanas <ec24bt023@iitdh.ac.in>
2026-09-24 22:41:44 +0530

    Fixed naming (memory -> register)

--- rtl/register file/register_file.v
+++ rtl/register file/register_file.v
@@ -20,7 +20,7 @@ module register_file #(
 );
 
     //Register size: DEPTH x DATA_WIDTH (eg. 256 x 32)
-    reg [DATA_WIDTH-1:0] memory [0:DEPTH-1];
+    reg [DATA_WIDTH-1:0] register [0:DEPTH-1];
 
 
     //Asynchronous read
@@ -30,7 +30,7 @@ module register_file #(
         for (i = 0; i < NUM_READ_PORTS; i=i+1) begin
 
             //Read data for each port using read_addr 
-            read_data[(i+1)*DATA_WIDTH-1 -: DATA_WIDTH] = memory[read_addr[(i+1)*ADDR_WIDTH-1 -: ADDR_WIDTH]];
+            read_data[(i+1)*DATA_WIDTH-1 -: DATA_WIDTH] = register[read_addr[(i+1)*ADDR_WIDTH-1 -: ADDR_WIDTH]];
         end
     end
 
@@ -43,17 +43,19 @@ module register_file #(
         //Reset
         if (rst) begin
             for (j = 0; j < DEPTH; j=j+1) begin
-                memory[j] <= {DATA_WIDTH{1'b0}};       //Reset memory -> Read ports also gets reset
+                register[j] <= {DATA_WIDTH{1'b0}};       //Reset register -> Read ports also gets reset
             end
         end
 
         //Synchronous write
         else begin
             for (k = 0; k < NUM_WRITE_PORTS; k=k+1) begin
+
+                //Port of higher value is written if multiple write ports are enabled for the same address
                 if (write_en[k]) begin
 
                     //Write data for each port using write_addr 
-                    memory[write_addr[(k+1)*ADDR_WIDTH-1 -: ADDR_WIDTH]] <= write_data[(k+1)*DATA_WIDTH-1 -: DATA_WIDTH];
+                    register[write_addr[(k+1)*ADDR_WIDTH-1 -: ADDR_WIDTH]] <= write_data[(k+1)*DATA_WIDTH-1 -: DATA_WIDTH];
                 end
             end
         end
